157ad90aa7ba4db203642d2c5efabd2e0dc7cd68
mngan <meilee.ngan@nist.gov>
2019-03-05 10:51:42 -0500

    Revert "Renaming folders"
    This reverts commit e88d8beb1901d5f15dad664084f96eacf5243347.

--- morph/frvt_morph.h
+++ morph/frvt_morph.h
@@ -3,7 +3,7 @@
  * Technology (NIST) by employees of the Federal Government in the course
  * of their official duties. Pursuant to title 17 Section 105 of the
  * United States Code, this software is not subject to copyright protection
- * and is in the public domain. NIST assumes no responsibility whatsoever for
+ * and is in the public domain. NIST assumes no responsibility  whatsoever for
  * its use by other parties, and makes no guarantees, expressed or implied,
  * about its quality, reliability, or any other characteristic.
  */
@@ -14,8 +14,8 @@
 #include <cstdint>
 #include <string>
 #include <vector>
-
-#include <frvt_structs.h>
+#include <memory>
+#include <iostream>
 
 namespace FRVT_MORPH {
 
@@ -24,10 +24,142 @@ enum class ImageLabel {
     /** Image type is unknown or unassigned */
     Unknown = 0,
     /** Non-scanned image */
-    NonScanned,
+    NonScanned = 1,
     /** Printed-and-scanned image */
-    Scanned
+    Scanned = 2
 };
+
+/**
+ * @brief
+ * Struct representing a single image
+ */
+typedef struct Image {
+    /** Number of pixels horizontally */
+    uint16_t width;
+    /** Number of pixels vertically */
+    uint16_t height;
+    /** Number of bits per pixel. Legal values are 8 and 24. */
+    uint8_t depth;
+    /** Managed pointer to raster scanned data.
+     * Either RGB color or intensity.
+     * If image_depth == 24 this points to  3WH bytes  RGBRGBRGB...
+     * If image_depth ==  8 this points to  WH bytes  IIIIIII */
+    std::shared_ptr<uint8_t> data;
+
+    Image() :
+        width{0},
+        height{0},
+        depth{24}
+        {}
+
+    Image(
+        uint16_t width,
+        uint16_t height,
+        uint8_t depth,
+        std::shared_ptr<uint8_t> &data
+        ) :
+        width{width},
+        height{height},
+        depth{depth},
+        data{data}
+        {}
+
+    /** @brief This function returns the size of the image data. */
+    size_t
+    size() const { return (width * height * (depth / 8)); }
+} Image;
+
+/**
+ * @brief
+ * Return codes for functions specified in this API
+ */
+enum class ReturnCode {
+    /** Success */
+    Success = 0,
+    /** Error reading configuration files */
+    ConfigError,
+    /** Elective refusal to process the input */
+    RefuseInput,
+    /** Involuntary failure to process the image */
+    ExtractError,
+    /** Cannot parse the input data */
+    ParseError,
+    /** Error occurred during the 1:1 match operation */
+    MatchError,
+    /** Unable to detect a face in the image */
+    FaceDetectionError,
+    /** Function is not implemented */
+    NotImplemented,
+    /** Vendor-defined failure */
+    VendorError
+};
+
+/** Output stream operator for a ReturnCode object. */
+inline std::ostream&
+operator<<(
+    std::ostream &s,
+    const ReturnCode &rc)
+{
+    switch (rc) {
+    case ReturnCode::Success:
+        return (s << "Success");
+    case ReturnCode::ConfigError:
+        return (s << "Error reading configuration files");
+    case ReturnCode::RefuseInput:
+        return (s << "Elective refusal to process the input");
+    case ReturnCode::ExtractError:
+        return (s << "Involuntary failure to process the image");
+    case ReturnCode::ParseError:
+        return (s << "Cannot parse the input data");
+    case ReturnCode::MatchError:
+        return (s << "Error occurred during the 1:1 match operation");
+    case ReturnCode::FaceDetectionError:
+        return (s << "Unable to detect a face in the image");
+    case ReturnCode::NotImplemented:
+        return (s << "Function is not implemented");
+    case ReturnCode::VendorError:
+        return (s << "Vendor-defined error");
+    default:
+        return (s << "Undefined error");
+    }
+}
+
+/**
+ * @brief
+ * A structure to contain information about a failure by the software
+ * under test.
+ *
+ * @details
+ * An object of this class allows the software to return some information
+ * from a function call. The string within this object can be optionally
+ * set to provide more information for debugging etc. The status code
+ * will be set by the function to Success on success, or one of the
+ * other codes on failure.
+ */
+typedef struct ReturnStatus {
+    /** @brief Return status code */
+    ReturnCode code;
+    /** @brief Optional information string */
+    std::string info;
+
+    ReturnStatus() {}
+    /**
+     * @brief
+     * Create a ReturnStatus object.
+     *
+     * @param[in] code
+     * The return status code; required.
+     * @param[in] info
+     * The optional information string.
+     */
+    ReturnStatus(
+        const ReturnCode code,
+        const std::string &info = ""
+        ) :
+        code{code},
+        info{info}
+        {}
+} ReturnStatus;
 
 /**
  * @brief
