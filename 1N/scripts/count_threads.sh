f6bf25e9d8d616fef490bffc1ebd856bbc1d84c8
mngan <meilee.ngan@nist.gov>
2019-12-03 15:30:36 -0500

    cleaning up nist-pages

--- 1N/scripts/count_threads.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-log=$1
-while true; do
-	top -H -b -n1 | grep validate1N | wc -l >> $log
-	sleep 10
-done
--- quality/scripts/count_threads.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-log=$1
-while true; do
-	top -H -b -n1 | grep validate_quality | wc -l >> $log
-	sleep 10
-done
